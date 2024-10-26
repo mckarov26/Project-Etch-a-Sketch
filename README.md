3ff84aadc2758b407b46ec205672cbea7b52bb5b
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2024-10-26 16:15:29 +1300

    modified README.md, index.html and script.js files.
    - Added what day it was finished in README.md file.
    - Added addtional buttons for features in index.html file
    - Added some functions and listener for interactive and behavior to the webpage.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 # HI!
 - On this Day October 25, 2024, I will start creating a pretty neat toy to flex my DOM manipulation skills.
+- Finished Project October 26, 2024. 
+- Will be adding some more features in the future when some ideas comes up!
--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const container = document.getElementById("container");
+const container = document.getElementById("container"); 
 const gridSize = document.getElementById("gridSize");
-const blackGrid = document.getElementById("blackGrid");
+const gridColor = document.getElementById("gridColor");
+const gridBlack = document.getElementById("gridBlack");
+const gridReset = document.getElementById("gridReset");
+const size = document.getElementById("size");
+
+// This function will return random color
+const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 256); // random value from 0-255 for red;
+    const g = Math.floor(Math.random() * 256); // random value from 0-255 for green;
+    const b = Math.floor(Math.random() * 256); // random value from 0-255 for blue;
+    return  `rgb(${r}, ${g}, ${b})`; // return rgb color as a string;
+}
+
+
+
+// This function will return black color
+const getBlackColor = () => {
+
+    return "rgb(0, 0, 0)";
+}
+
+
+// this function will return as a description on webpage of what size it is.
+
+const sizeDescription = (squaresPerSide) => {
+    size.textContent = `${squaresPerSide} x ${squaresPerSide} Grid `;
+}
 
 
 
 
 
-
-
-
-
-
+// This function is for creating the size and will show inside the container. 
 const createGrid = (size) => {
 
     container.innerHTML = "";
+
+    
     
     for (let i = 0; i < size * size; i++) {
            
@@ -21,34 +45,60 @@ const createGrid = (size) => {
         divCell.classList.add("cell");
         container.appendChild(divCell);
 
-
-
-        divCell.addEventListener("mouseover", () => {
-            divCell.style.backgroundColor = "black";
-            
-        })
-
-        divCell.addEventListener("mouseleave", () => {
-            divCell.style.backgroundColor = "lightgrey"
-            divCell.style.transition ="background-color 3s"
-        })
-
+       
         }
        
     
 }   
-
+// This is for asking user how many number of squares per side for the new grid from 1-100
 gridSize.addEventListener("click", () => {
     let squaresPerSide = prompt("Choose 1-100 for the number of squares per side for the new grid:");
     squaresPerSide = parseInt(squaresPerSide);
+
     document.documentElement.style.setProperty("--size", squaresPerSide)
 
     if (!isNaN(squaresPerSide) && squaresPerSide >= 1 && squaresPerSide <= 100) {
         createGrid(squaresPerSide);
+        sizeDescription(squaresPerSide);
     } else {
         alert("Please enter a valid NUMBER between 1 and 100");
 
     }
+})
+
+// This is to add colors on grid when hover
+gridColor.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell")
+
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = getRandomColor();
+        })
+    })      
+       
+   
+
+})
+
+// This is to add black color on grid when hover
+gridBlack.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = getBlackColor();
+        } )
+    })
+})
+
+
+//This is to reset the color from it default
+
+gridReset.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "lightgrey";
+    })
 })
 
 
